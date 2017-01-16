fcc3bf62650dfb0c449226609160f05f87270cb8
jlamim <dev@jlamim.com.br>
2017-01-16 19:12:33 -0200

    SQL da tabela categoria

---
+++ categoria.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `categoria` (
+`idCategoria` int(11) NOT NULL AUTO_INCREMENT,
+`nomeCategoria` varchar(45) NOT NULL,
+PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
